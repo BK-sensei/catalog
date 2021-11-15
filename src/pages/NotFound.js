35564b6452e6ab55767069b2edca575021ba5b10
becca <rebecca.kaci@gmail.com>
2021-11-15 14:41:30 +0100

    création de la page error 404

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -4,7 +4,9 @@ class NotFound extends Component {
     render() {
         return (
             <div>
-                
+                <h1>ERROR 404</h1>
+                <p>This page does not longer exist</p>
+                <Link className="nav-link" to="/">Back to Homepage</Link>
             </div>
         );
     }
